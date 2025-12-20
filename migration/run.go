ae25c9e8b42bf89d3a88447c3c0a95ad9f8a54be
Ajii <ajiedafuq@gmail.com>
2025-12-20 19:52:01 +0700

    fix config migration

--- migration/run.go
+++ migration/run.go
@@ -4,22 +4,28 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/skyapps-id/casdoor-test/migrations"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using OS env")
+	}
+
 	certBytes, err := os.ReadFile("./cert.pem")
 	if err == nil {
 		log.Println("📄 Certificate loaded from cert.pem")
 	}
 	// Load konfigurasi dari environment variables atau config file
 	config := migrations.CasdoorConfig{
-		Endpoint:         getEnv("CASDOOR_ENDPOINT", "http://localhost:8000"),
-		ClientID:         getEnv("CASDOOR_CLIENT_ID", "26054a2dfb593fa0990c"),
-		ClientSecret:     getEnv("CASDOOR_CLIENT_SECRET", "520986d9f34ddcee617d9dfe6c4e203c97c5bc04"),
-		Certificate:      getEnv("CASDOOR_CERTIFICATE", string(certBytes)), // Path ke certificate file atau certificate string
-		OrganizationName: getEnv("CASDOOR_ORGANIZATION", "skyapps"),
-		ApplicationName:  getEnv("CASDOOR_APPLICATION", "application_i9irbv"),
+		Endpoint:         getEnv("CASDOOR_ENDPOINT", ""),
+		ClientID:         getEnv("CASDOOR_CLIENT_ID", ""),
+		ClientSecret:     getEnv("CASDOOR_CLIENT_SECRET", ""),
+		Certificate:      getEnv("CASDOOR_CERTIFICATE", string(certBytes)),
+		OrganizationName: getEnv("CASDOOR_ORGANIZATION", ""),
+		ApplicationName:  getEnv("CASDOOR_APPLICATION", ""),
 	}
 
 	// Buat instance migration
